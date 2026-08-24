fd4b32e92d29534e13a1a1bc586a3f859917e74c
noor <gurnoor.bola@gmail.com>
2026-08-23 22:20:28 -0700

    helicopter: fixed server start and setup

--- lua/helicopter/ask.lua
+++ lua/helicopter/ask.lua
@@ -1,21 +1,18 @@
 local M = {}
 
-local utils = require("helicopter.utils")
-local agent = require("helicopter.agent")
+local Utils = require("helicopter.utils")
+local Agent = require("helicopter.agent")
 
 -- TODO: update this to build better prompts
 local function build_prompt(query, lines)
-	local prompt = "Query: " .. query .. "\n Context:" .. utils.flatten_str_arr(lines)
+	local prompt = "Query: " .. query .. "\n Context:" .. Utils.flatten_str_arr(lines)
 	return prompt
 end
 
 function M.select_and_ask(opts)
-	local lines = utils.get_lines(opts.line1, opts.line2)
-	local query = utils.prompt_input()
+	local lines = Utils.get_lines(opts.line1, opts.line2)
+	local query = Utils.prompt_input()
 	local prompt = build_prompt(query, lines)
-
-	-- local seq_session = agent.Session.seq:new()
-	-- seq_session:prompt()
 
 	local response = agent.send_message(prompt)
 	print(response)
