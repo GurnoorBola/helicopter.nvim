74068df466f27ca511f2c505cc0620a669541fa6
noor <gurnoor.bola@gmail.com>
2026-08-23 20:37:55 -0700

    agent: add annotations and make session sequential

--- lua/helicopter/agent.lua
+++ lua/helicopter/agent.lua
@@ -13,9 +13,11 @@ local utils = require("helicopter.utils")
 local MAX_REQUESTS = 1000
 local JSON_RPC_VERSION = "2.0"
 
+---@type table<number, Callback>
 local callbacks = {}
 
 -- Maps session id to session object
+---@type table<string, Session>
 local active_sesssions = {}
 
 local curr_id = 0
@@ -37,13 +39,16 @@ local function parse_response(str_response)
 	print("[Error] Code:", json_response.error.code, "Message:", json_response.error.message)
 end
 
-local function register_callback(request_id, on_response)
-	callbacks[request_id] = on_response
+local function register_callback(request_id, callback)
+	callbacks[request_id] = callback
 end
 
-local function send_request(json_request, on_response)
-	if on_response then
-		register_callback(json_request.id, on_response)
+---@param json_request JsonObject
+---@param callback Callback
+---@return number
+local function send_request(json_request, callback)
+	if callback then
+		register_callback(json_request.id, callback)
 	end
 	local str_request = json.encode(json_request) .. "\n"
 	return vim.fn.chansend(M.id, str_request)
@@ -101,6 +106,7 @@ function M.stop_server()
 end
 
 -- Initializes the agent and executes callback if all checks pass
+---@param callback Callback
 function M.initialize(callback)
 	local json_request = build_request("initialize", config.initialize_request)
 	send_request(json_request, function(json_response)
@@ -109,6 +115,8 @@ function M.initialize(callback)
 	end)
 end
 
+---@param method_id string
+---@param callback Callback
 function M.authenticate(method_id, callback)
 	local json_request = build_request("authenticate", { method_id = method_id })
 	send_request(json_request, function(json_response)
@@ -118,116 +126,84 @@ function M.authenticate(method_id, callback)
 	end)
 end
 
--- Session object that is produced from the new function
---
--- Using functions from the Session.seq table will
--- implement sequential semantics on the session object
+---@alias JsonObject table<string, any>
+---@alias Callback fun(json_response:JsonObject)
 
--- To be a sequential command means that any command that was executed before
--- it must finish its callback before running this command
---
--- If a sequential command queue is empty place a sentinel and execute command
--- If a sequential command queue is not empty place command on queue
---
--- then it should run callback
--- then it should pop top and run next command
---
---
+--- An ACP Session
+---@class Session
+---@field public id string
+---@field private _updates table[]
+---@field private _callbacks Callback[]
+---@field private _queue Queue
 M.Session = {}
-M.Session.seq = {}
 
+---@param params JsonObject
+---@param callback Callback
+---@return Session
 function M.Session:new(params, callback)
 	local new_session = {
+		id = "unset",
 		_updates = {},
 		_callbacks = {},
-
-		seq = {
-			_queue = utils.queue:new(),
-		},
+		_queue = utils.queue:new(),
 	}
-	new_session.seq.session = new_session
 
 	setmetatable(new_session, { __index = self })
-	setmetatable(new_session.seq, { __index = self.seq })
+
+	new_session._queue:push("~")
 
 	local json_request = build_request("session/new", params)
 
 	send_request(json_request, function(json_response)
 		-- TODO: setup the session object
-		new_session.id = json_response.sessionId
-		active_sesssions[new_session.id] = new_session
-		callback(json_response)
-	end)
-	return new_session
-end
-
--- Returns a session.seq
--- Creates a dummy marker to show an in progress session being initialized
--- session object can still be operated on and will work as expected
-function M.Session.seq:new(params, callback)
-	local new_session = {
-		_updates = {},
-		_callbacks = {},
-
-		seq = {
-			_queue = utils.queue:new(),
-		},
-	}
-	new_session.seq.session = new_session
-
-	setmetatable(new_session, { __index = self })
-	setmetatable(new_session.seq, { __index = self.seq })
-
-	new_session.seq._queue:push("~")
-
-	local json_request = build_request("session/new", params)
-
-	send_request(json_request, function(json_response)
-		new_session.seq._queue:pop()
+		new_session._queue:pop()
 
 		new_session.id = json_response.sessionId
 		active_sesssions[new_session.id] = new_session
 		callback(json_response)
 
-		if new_session.seq._queue:size() ~= 0 then
+		if not new_session._queue:empty() then
 			local next = new_session.seq._queue:peek()
 			next()
 		end
 	end)
 
-	return new_session.seq
+	return new_session
 end
 
+---@param prompt JsonObject
+---@param callback Callback
+---@return self
 function M.Session:prompt(prompt, callback)
-	local json_request = build_request("session/prompt", { sessionId = self.id, prompt = prompt })
-	send_request(json_request, function(json_response)
-		callback(json_response)
-	end)
-end
-
-function M.Session.seq:prompt(prompt, callback)
-	local session = self.session
-	local cmd = function()
-		session:prompt(prompt, function(json_response)
+	local prompt_cmd = function()
+		local json_request = build_request("session/prompt", { sessionId = self.id, prompt = prompt })
+		send_request(json_request, function(json_response)
 			self._queue:pop()
+
 			callback(json_response)
-			if self._queue:size() ~= 0 then
+
+			if not self._queue:empty() then
 				local next = self._queue:peek()
 				next()
 			end
 		end)
 	end
-	self._queue:push(cmd)
+	self._queue:push(prompt_cmd)
 	if self._queue:empty() then
-		cmd()
+		prompt_cmd()
 	end
 	return self
 end
 
+---@param type string
+---@param callback Callback
+---@return self
 function M.Session:on_session_update(type, callback)
 	self._callbacks[type] = callback
+	return self
 end
 
+---@param update JsonObject
 function M.Session:_update(update)
 	table.insert(self._updates, update)
 	-- TODO: switch on update type and call appropriate handler
